06942fb03e64219e53c84a91d51bedd43a365c70
Akshay sharma <118508688+AkshaySharma14@users.noreply.github.com>
2025-08-27 15:34:34 +0530

    Update OnlineDelivery.jsx

--- src/components/OnlineDelivery.jsx
+++ src/components/OnlineDelivery.jsx
@@ -1,48 +1,34 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Card from './Card';
 
 export default function OnlineDelivery() {
-    const [data, setData] = useState([]);
-    const componentRef = useRef(null);
-    const [isAtTop, setIsAtTop] = useState(false);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (componentRef.current) {
-                const rect = componentRef.current.getBoundingClientRect();
-                setIsAtTop(rect.top <= 0);
-            }
-        };
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+  const fetchTopRestaurant = async () => {
+    const response = await fetch('/restaurantChains.json');
+    const apiData = await response.json();
+    setData(apiData);
+  };
 
-    const fetchTopRestaurant = async () => {
-        const response = await fetch('/restaurantChains.json')
-        const apiData = await response.json();
-        setData(apiData);
-    }
-    useEffect(() => {
-        fetchTopRestaurant();
-    }, []);
+  useEffect(() => {
+    fetchTopRestaurant();
+  }, []);
 
-    return (
-        <div className='max-w-[1200px] mx-auto' ref={componentRef}>
-            {/* Header */}
-            <div className='flex my-3 items-center justify-between'>
-                <div className='text-[25px] font-bold'>
-                    Restaurant with Online food Delivery in Jodhpur
-                </div>
-            </div>  
-
-            {/* Restaurant cards */}
-            <div className='grid grid-cols-4 gap-3'>
-                {data.map((d, i) => (
-                    <Card key={i} {...d} />
-                ))}
-            </div>
+  return (
+    <div className="max-w-[1200px] mx-auto">
+      {/* Header */}
+      <div className="flex my-3 items-center justify-between">
+        <div className="text-[25px] font-bold">
+          Restaurant with Online food Delivery in Jodhpur
         </div>
-    )   
+      </div>
+
+      {/* Restaurant cards */}
+      <div className="grid grid-cols-4 gap-3">
+        {data.map((d, i) => (
+          <Card key={i} {...d} />
+        ))}
+      </div>
+    </div>
+  );
 }
